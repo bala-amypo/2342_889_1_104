74bd92473738025d87bb71c37dbf8e1abd644aa6
bala-amypo <bala@amypo.in>
2025-12-18 08:41:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
+import com.example.demo.service.EmployeeProfileService;
 
 @Service
 public class EmployeeProfileServiceImpl implements EmployeeProfileService{
