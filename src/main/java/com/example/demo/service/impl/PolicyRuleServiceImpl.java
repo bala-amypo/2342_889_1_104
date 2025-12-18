0e279a2bdaf1afcc6dcdae5a39de5db5476c70be
bala-amypo <bala@amypo.in>
2025-12-18 16:53:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -6,3 +6,16 @@ import com.example.demo.model.PolicyRule;
 import com.example.demo.repository.PolicyRuleRepository;
 import com.example.demo.service.PolicyRuleService;
 
+
+@Service
+public PolicyRuleServiceImpl implements PolicyRuleService{
+    @Autowired
+    private PolicyRuleRepository rep;
+    public PolicyRuleServiceImpl(PolicyRuleRepository rep){
+        this.rep=rep;
+    }
+    @Override
+    public PolicyRule createRule(PolicyRule rule){
+        
+    }
+}
