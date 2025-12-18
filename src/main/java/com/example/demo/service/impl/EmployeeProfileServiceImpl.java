dba30cb3d7ef2e1ee24c759a24043b2de350fc53
bala-amypo <bala@amypo.in>
2025-12-18 07:58:27 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -6,15 +6,24 @@ import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
 
 @Service
-public class EmployeeProfile
-private EmployeeProfileRepository rep;
+public class EmployeeProfileServiceImpl implements EmployeeProfileService{
+    @Autowired
+    private EmployeeProfileRepository rep;
+    public EmployeeProfileServiceImplw(EmployeeProfileRepository rep){
+        this.rep=rep;
+    }
+    @Override
+    public EmployeeProfile savedata(EmployeeProfile emp){
+        return rep.save(emp);
+    }
 
-@Override
-public EmployeeProfile savedata(EmployeeProfile emp){
-    return rep.save(emp);
-}
+    @Override
+    public EmployeeProfile id(Long id){
+        return rep.findById(id);
+    }
 
-@Override
-public EmployeeProfile id(Long id){
-    return rep.findById(id);
+    @Override
+    public EmployeeProfile update(Long id,EmployeeProfile emp){
+        EmployeeProfile existing = re
+    }
 }
