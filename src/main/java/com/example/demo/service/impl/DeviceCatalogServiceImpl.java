62b81b0f6f6d491094aa2d9306e690527652ddd5
bala-amypo <bala@amypo.in>
2025-12-18 16:11:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceCatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceCatalogServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.model.EmployeeProfile;
+import com.example.demo.model.DeviceCatalogItem;
 import com.example.demo.repository.DeviceCatalogItemRepository;
 import com.example.demo.service.DeviceCatalogService;
 
@@ -10,4 +10,20 @@ import com.example.demo.service.DeviceCatalogService;
 public DeviceCatalogServiceImpl implements DeviceCatalogService{
     @Autowired
     private DeviceCatalogItemRepository rep;
+    public DeviceCatalogServiceImpl(DeviceCatalogItemRepository rep){
+        this.rep=rep;
+    }
+    @Override
+    public DeviceCatalogItem createItem(DeviceCatalogItem item{
+        return src.save(item);
+    }
+    @Override
+    public DeviceCatalogItem updateActiveStatus(Long id,boolean active){
+        EmployeeProfile existing = rep.findById(id).orElse(null);
+        if(existing!=null){
+            existing.setActive(active);
+            return rep.save(existing);
+        }
+        return null;
+    }
 }
