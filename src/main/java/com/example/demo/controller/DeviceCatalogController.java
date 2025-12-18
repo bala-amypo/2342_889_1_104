1ef0a6d6a31e5c52dd3eee2c4a7adebe76fb53d1
bala-amypo <bala@amypo.in>
2025-12-18 16:17:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/DeviceCatalogController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+import com.example.demo.model.DeviceCatalogItem;
+import com.example.demo.service.DeviceCatalogService;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/devices")
+public class DeviceCatalogController{
+    @A
+}
