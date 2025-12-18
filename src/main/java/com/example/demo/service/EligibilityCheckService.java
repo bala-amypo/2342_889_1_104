d6b9d2f7a3152e35e207ebc35268ecec94572e40
bala-amypo <bala@amypo.in>
2025-12-18 15:41:32 +0000

    User commit

--- src/main/java/com/example/demo/service/EligibilityCheckService.java
+++ src/main/java/com/example/demo/service/EligibilityCheckService.java
@@ -1,2 +1,6 @@
 package com.example.demo.service;
-import com.example.demo.model.Eligibit;
+import com.example.demo.model.EligibilityCheckRecord;
+public interface EligibilityCheckService{
+    EligibilityCheckRecord validateEligibility(Long employeeId,Long deviceItemId);
+    EligibilityCheckRecord getCheckByEmployee(Long employeeId);
+}
