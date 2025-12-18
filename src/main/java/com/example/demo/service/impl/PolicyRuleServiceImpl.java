0d40989a89c7e0a0d320cd1819e81b3941325089
bala-amypo <bala@amypo.in>
2025-12-18 16:50:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.PolicyRule;
+import com.example.demo.repository.PolicyRuleRepository;
+import com.example.demo.service.PolicyRuleService;
+
