9a8e197074c0a53ead6900417074c07cde4fc428
bala-amypo <bala@amypo.in>
2025-12-18 06:38:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeProfileController.java
+++ src/main/java/com/example/demo/controller/EmployeeProfileController.java
@@ -1,3 +1,14 @@
 package com.example.demo.controller;
 import com.example.demo.model.EmployeeProfile;
-import com.example.demo.service.EmployeeProfileService
+import com.example.demo.service.EmployeeProfileService;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/employees")
+
+public class EmployeeProfileController{
+    private final EmployeeProfileService
+}
