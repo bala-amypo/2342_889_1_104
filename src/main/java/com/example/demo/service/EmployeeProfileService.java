2ac0528024d28c893a871564f65be84693890394
bala-amypo <bala@amypo.in>
2025-12-18 07:38:25 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeProfileService.java
+++ src/main/java/com/example/demo/service/EmployeeProfileService.java
@@ -2,5 +2,6 @@ package com.example.demo.service;
 import com.example.demo.model.EmployeeProfile;
 public interface EmployeeProfileService{
     EmployeeProfile savedata(EmployeeProfile emp);
-    EmployeeProfile i
+    EmployeeProfile id(Long id);
+    EmployeeProfile 
 }
