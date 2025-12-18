1a495de93d742ce47b282d4992a36020c19a0b56
bala-amypo <bala@amypo.in>
2025-12-18 09:39:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.EmployeeProfile;
+import com.example.demo.model.EligibilityCheckRecord;
 @Repository
-public interface EligibiRepository extends JpaRepository<EmployeeProfile,Long>{
+public interface EligibilityCheckRecordRepository extends JpaRepository<EligibilityChechRecord,Long>{
 }
