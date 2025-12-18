f878a1c5bbbe588f96b13fa8ed4ce8a3f74cf87d
bala-amypo <bala@amypo.in>
2025-12-18 01:57:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/EmployeeProfile.java
+++ src/main/java/com/example/demo/entity/EmployeeProfile.java
@@ -9,4 +9,18 @@ public class EmployeeProfile{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
-}
+    @NotBlank(message="Should not contain blank spaces")
+    @Column(unique=true)
+    private String employeeID;
+    @NotBlank(message="Should not contain blank spaces")
+    private String fullName;
+    @Email(message="Invalid format")
+    @NotBlank(message="Should not contain blank spaces")
+    @Column
+    private String email;
+    @NotBlank(message="Should not contain blank spaces")
+    private String department;
+    @NotBlank(message="Should not contain blank spaces")
+    private String jobRole;
+    private Boolean active;
+    private LocalDateTime createAt;
