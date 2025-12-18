375e5cfc1ad110394b0f7472f6fb8eb531a8fe5e
bala-amypo <bala@amypo.in>
2025-12-18 16:26:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/IssuedDeviceRecordService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.impl;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.IssuedDeviceRecord;
+import com.example.demo.repository.IssuedDeviceRecordRepository;
+import com.example.demo.service.IssuedDeviceService;
