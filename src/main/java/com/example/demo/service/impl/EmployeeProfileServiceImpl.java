1e853607b6358e44f3702ac63ef2cfa44aaf0b3e
bala-amypo <bala@amypo.in>
2025-12-18 07:48:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -3,4 +3,18 @@ package com.example.demo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.EmployeeProfile;
-import com.example.demo.repository.EmployeeProfile
+import com.example.demo.repository.EmployeeProfileRepository;
+
+@Service
+public class
+private EmployeeProfileRepository rep;
+
+@Override
+public EmployeeProfile savedata(EmployeeProfile emp){
+    return rep.save(emp);
+}
+
+@Override
+public EmployeeProfile id(Long id){
+    return rep.findById(id);
+}
