3e10928c213f8aeaef4e13c9e62df416a652b3d1
bala-amypo <bala@amypo.in>
2025-12-18 10:14:01 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -1,1 +1,10 @@
 package com.example.demo.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name="Device_CatalogItem")
+public class DeviceCatalogItem{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+}
