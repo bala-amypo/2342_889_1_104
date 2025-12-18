04c1050cd849bd405a1e13fca54d13b93adff9de
bala-amypo <bala@amypo.in>
2025-12-18 10:40:56 +0000

    User commit

--- src/main/java/com/example/demo/model/PolicyRule.java
+++ src/main/java/com/example/demo/model/PolicyRule.java
@@ -16,4 +16,51 @@ public class PolicyRule{
     private String appliesToRole;
     private Integer maxDevicesAllowed;
     private Boolean active; 
+    public PolicyRule(){}
+    public PolicyRule(String ruleCode, String description, String appliesToDepartment, String appliesToRole,
+        Integer maxDevicesAllowed, Boolean active) {
+        this.ruleCode = ruleCode;
+        this.description = description;
+        this.appliesToDepartment = appliesToDepartment;
+        this.appliesToRole = appliesToRole;
+        this.maxDevicesAllowed = maxDevicesAllowed;
+        this.active = active;
+    }
+    public String getRuleCode() {
+        return ruleCode;
+    }
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getAppliesToDepartment() {
+        return appliesToDepartment;
+    }
+    public void setAppliesToDepartment(String appliesToDepartment) {
+        this.appliesToDepartment = appliesToDepartment;
+    }
+    public String getAppliesToRole() {
+        return appliesToRole;
+    }
+    public void setAppliesToRole(String appliesToRole) {
+        this.appliesToRole = appliesToRole;
+    }
+    public Integer getMaxDevicesAllowed() {
+        return maxDevicesAllowed;
+    }
+    public void setMaxDevicesAllowed(Integer maxDevicesAllowed) {
+        this.maxDevicesAllowed = maxDevicesAllowed;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    
 }
