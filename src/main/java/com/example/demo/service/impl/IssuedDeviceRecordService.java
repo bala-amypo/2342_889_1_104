257c9a853a70ae5b2f34740079bd419269fecdef
bala-amypo <bala@amypo.in>
2025-12-18 16:29:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/IssuedDeviceRecordService.java
+++ src/main/java/com/example/demo/service/impl/IssuedDeviceRecordService.java
@@ -5,3 +5,16 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.IssuedDeviceRecord;
 import com.example.demo.repository.IssuedDeviceRecordRepository;
 import com.example.demo.service.IssuedDeviceService;
+
+@Service
+public IssuedDeviceServiceImpl implements IssuedDeviceService{
+    @Autowired
+    private IssuedDeviceRecordRepository rep;
+    public IssuedDeviceServiceImpl(IssuedDeviceRecordRepository rep){
+        this.rep=rep;
+    }
+    @Override
+    public IssuedDeviceRecord issueDevice(IssuedDeviceRecord record){
+        return rep.save(record);
+    }
+}
