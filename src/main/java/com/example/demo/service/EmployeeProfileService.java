59e974c95d937635dbeac2cc10569de181303283
bala-amypo <bala@amypo.in>
2025-12-18 07:43:25 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeProfileService.java
+++ src/main/java/com/example/demo/service/EmployeeProfileService.java
@@ -3,5 +3,5 @@ import com.example.demo.model.EmployeeProfile;
 public interface EmployeeProfileService{
     EmployeeProfile savedata(EmployeeProfile emp);
     EmployeeProfile id(Long id);
-    EmployeeProfile 
+    EmployeeProfile update(Long id,EmployeeProfile emp);
 }
---
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.EmployeeProfile;
+import com.example.demo.repository.EmployeeProfile
