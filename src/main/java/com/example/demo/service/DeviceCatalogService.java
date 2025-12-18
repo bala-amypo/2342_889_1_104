65b50d3aaa2f9fa3e411bf746f33676273300f8d
bala-amypo <bala@amypo.in>
2025-12-18 15:32:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DeviceCatalogService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+import com.example.demo.model.DeviceCatalogItem;
+public interface DeviceCatalogService{
+    DeviceCatalogItem createItem(DeviceCatalogItem item);
+    DeviceCatalogItem updateActiveStatus(Long id,boolean active);
+    List<DeviceCatalogItem> getAllItems();
+}
--- src/main/java/com/example/demo/service/EmployeeProfileService.java
+++ src/main/java/com/example/demo/service/EmployeeProfileService.java
@@ -3,6 +3,6 @@ import com.example.demo.model.EmployeeProfile;
 public interface EmployeeProfileService{
     EmployeeProfile createEmployee(EmployeeProfile employee);
     EmployeeProfile getEmployeeById(Long id);
-    
+    List<EmployeeProfile> getAllEmployees();
     EmployeeProfile updateEmployeeStatus(Long id,boolean active);
 }
