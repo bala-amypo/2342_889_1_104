e0484b5f94487f3a25a9269c94bc44e76ff7a577
bala-amypo <bala@amypo.in>
2025-12-18 16:02:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/DeviceCatalogServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.impl;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.EmployeeProfile;
+import com.example.demo.repository.DeviceCatalogItemRepository;
+import com.example.demo.service.EmployeeProfileService;
