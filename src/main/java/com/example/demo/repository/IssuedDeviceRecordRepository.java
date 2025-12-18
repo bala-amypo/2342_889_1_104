ebdbc73f186e84c4cdb8e5538890d7dee40381dc
bala-amypo <bala@amypo.in>
2025-12-18 09:41:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/IssuedDeviceRecordRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.IssuedDeviceRecord;
+@Repository
+public interface IssuedDeviceRecordRepository extends JpaRepository<IssuedDeviceRecord,Long>{
+}
