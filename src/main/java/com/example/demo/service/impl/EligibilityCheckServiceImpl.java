1e608167c699c53f64698e4116520b7071943538
bala-amypo <bala@amypo.in>
2025-12-18 17:23:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.EligibilityCheckRecord;
 import com.example.demo.repository.EligibilityCheckRecordRepository;
 import com.example.demo.service.EligibilityCheckService;
+import com.example.demo.repository.EmployeeProfileRepository;
 
 @Service
 public EligibilityCheckServiceImpl implements EligibilityCheckService{
     @Autowired
-    Employeep
+    EmployeeProfileRepository rep;
     @Autowired
     private EligibilityCheckRecordRepository rep;
     public EligibilityCheckServiceImpl (EligibilityCheckRecordRepository rep){
@@ -20,7 +21,7 @@ public EligibilityCheckServiceImpl implements EligibilityCheckService{
         EligibilityCheckRecord record=new EligibilityCheckRecord();
         record.setEmployeeId(employeeId);
         record.setDeviceItemId(deviceItemId);
-        Employee
+        EmployeeRepository employee=employeeRepository.findById(employeeId).orElse(null);
     }
     @Override
     public EligibilityCheckRecord getCheckByEmployee(Long employeeId){
