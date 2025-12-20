ae968fda6164f427cacc6602a033c096444fcdec
bala-amypo <bala@amypo.in>
2025-12-20 15:54:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.EligibilityCheckRecord;
 import com.example.demo.repository.EligibilityCheckRecordRepository;
-import com.example.demo.service.EligibilityCheckRecordService;
+import com.example.demo.service.EligibilityCheckService;
 
 @Service
 public class EligibilityCheckServiceImpl implements EligibilityCheckService{
